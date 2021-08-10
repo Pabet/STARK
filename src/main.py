16363ad9f8b4d86cce6a0234e21dc7cf8926065a
Patrick Bettermann <patrick.bettermann@gmx.de>
2021-08-10 15:33:28 +0200

    initial commit

--- src/main.py
+++ src/main.py
@@ -1,11 +1,4 @@
 from pyfinite import ffield
-F = ffield.FField(5) # create the field GF(2^5)
-a = 7    # field elements are denoted as integers from 0 to 2^5-1
-b = 15
-F.ShowPolynomial(a) # show the polynomial representation of a
-'x^2 + x^1 + 1'
-c = F.Multiply(a,b) # multiply a and b modulo the field generator
-F.ShowPolynomial(c)
 
 
 def print_hi(name):
@@ -16,5 +9,9 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    F = ffield.FField(5)  # create the field GF(2^5)
+    a = 7  # field elements are denoted as integers from 0 to 2^5-1
+    b = 15
+    F.ShowPolynomial(a)  # show the polynomial representation of a
+    c = F.Multiply(a, b)  # multiply a and b modulo the field generator
+    F.ShowPolynomial(c)
